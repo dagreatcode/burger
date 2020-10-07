c3769143d54b50fe21ccba28c8d5d098aad402fc
dagreatcode <dagreatcode@gmail.com>
2020-10-07 10:02:21 -0400

    fixing up

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = require ("express");
+const express = require("express");
+const exphb = require("express-handlebars");
+const mysql = require("mysql");
+// const connection = require("./db/connection");
+
+const app = express;
+
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+
