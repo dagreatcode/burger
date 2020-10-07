84f57ae84bf19f5f849d1a4d980715015c27f769
dagreatcode <dagreatcode@gmail.com>
2020-10-07 08:44:11 -0400

    WORKING ON the schemas data BASE page

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
--- Create the burgers_db.
--- Switch to or use the burgers_db.
--- Create a burgers table with these fields:
--- id: an auto incrementing int that serves as the primary key.
--- burger_name: a string.
--- devoured: a boolean.
+CREATE DATABASE burgers_db;
+USE burgers_db.
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(45) NULL,
+devoured BOOLEAN NOT NULL,
+);
