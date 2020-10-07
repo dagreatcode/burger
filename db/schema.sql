3682dc03b131d055997f31a588a86e82d1c943f3
dagreatcode <dagreatcode@gmail.com>
2020-10-07 08:55:29 -0400

    Lets get started on the/ didnt send the last commit, fixed the database files to import, time to send

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE burgers_db;
 USE burgers_db.
 CREATE TABLE burgers (
-id INT NOT NULL AUTO_INCREMENT,
-burger_name VARCHAR(45) NULL,
-devoured BOOLEAN NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(45) NULL,
+    devoured BOOLEAN NOT NULL,
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,11 @@
 -- In this file, write insert queries to populate the burgers table with at least three entries.
+INSERT INTO burgers (id, burger, devoured)
+VALUES (1, "Plain Jain", TRUE);
+
+INSERT INTO burgers (id, burger, devoured)
+VALUES (2, "Mondow", TRUE);
+
+INSERT INTO burgers (id, burger, devoured)
+VALUES (3, "Good Burger", TRUE);
 
 -- Run the schema.sql and seeds.sql files into the mysql server from the command line
