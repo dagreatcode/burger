1abb12cadd575e880ef95f8387ed38c126db9e34
dagreatcode <dagreatcode@gmail.com>
2020-10-07 08:34:18 -0400

    creating the data BASE

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+-- In this file, write insert queries to populate the burgers table with at least three entries.
+
+-- Run the schema.sql and seeds.sql files into the mysql server from the command line
