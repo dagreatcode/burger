8a3d19cd4ec6ee508146acbcdcaebd731d681516
dagreatcode <dagreatcode@gmail.com>
2020-10-07 09:01:55 -0400

    WORKING ON the seeds data BASE page

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,4 +8,11 @@ VALUES (2, "Mondow", TRUE);
 INSERT INTO burgers (id, burger, devoured)
 VALUES (3, "Good Burger", TRUE);
 
+-- * means all
+SELECT * FROM burgers;
+-- Updates the row where the column name is Good Burger --
+UPDATE burgers
+SET burger_name = Mondow, devoured = true
+WHERE name = "Good Burger";
+
 -- Run the schema.sql and seeds.sql files into the mysql server from the command line
