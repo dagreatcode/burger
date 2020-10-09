7a32f9c6fdcf89edf765317f812bd4c7f158f2cb
dagreatcode <dagreatcode@gmail.com>
2020-10-09 18:38:52 -0400

    added my middleware

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const exphb = require("express-handlebars");
+const exphbs = require("express-handlebars");
 const mysql = require("mysql");
 // const connection = require("./db/connection");
 
@@ -9,3 +9,16 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
