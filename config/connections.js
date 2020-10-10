34874cb22c1b8f89b26df06b00131c5aa68d4df1
dagreatcode <dagreatcode@gmail.com>
2020-10-09 23:30:37 -0400

    last

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+// var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Family1st",
-  database: "burgrs_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Family1st",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -18,5 +23,6 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+// // Export connection for our ORM to use.
+// connection.connect();
 module.exports = connection;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,24 @@
--- -- In this file, write insert queries to populate the burgers table with at least three entries.
--- INSERT INTO burgers (id, burger_name, devoured)
--- VALUES (1, "Plain Jain", TRUE);
+-- In this file, write insert queries to populate the burgers table with at least three entries.
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "Plain Jain", TRUE);
 
--- INSERT INTO burgers (id, burger_name, devoured)
--- VALUES (2, "Mondow", TRUE);
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (2, "Mondow", TRUE);
 
--- INSERT INTO burgers (id, burger_name, devoured)
--- VALUES (3, "Good Burger", TRUE);
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (3, "Good Burger", TRUE);
 
--- -- * means all
--- SELECT * FROM burgers;
--- -- Updates the row where the column name is Good Burger --
--- UPDATE burgers
--- SET burger_name = Mondow, devoured = true
--- WHERE name = "Good Burger";
+-- * means all
+SELECT * FROM burgers;
+-- Updates the row where the column name is Good Burger --
+UPDATE burgers
+SET burger_name = Mondow, devoured = true
+WHERE name = "Good Burger";
 
--- Run the schema.sql and seeds.sql files into the mysql server from the command line
-INSERT INTO burgers (burger_name) VALUES ('Plain Jain');
-INSERT INTO burgers (burger_name) VALUES ('Mondow');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Left', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Right', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Middle', true);
-INSERT INTO burgers (burger_name) VALUES ('Good Burger');
+-- -- Run the schema.sql and seeds.sql files into the mysql server from the command line
+-- INSERT INTO burgers (burger_name) VALUES ('Plain Jain');
+-- INSERT INTO burgers (burger_name) VALUES ('Mondow');
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Left', true);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Right', true);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Middle', true);
+-- INSERT INTO burgers (burger_name) VALUES ('Good Burger');
